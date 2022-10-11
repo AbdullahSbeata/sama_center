3e471ccd1b74b6a0e625099027eae6214427fbf7
abdullah sbeata <abedalla.102030@gmail.com>
2022-10-11 06:42:49 +0300

    Sama Center

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:sama/bottom navegation bar.dart';
 
 class SplashScreen extends StatefulWidget {
-  const SplashScreen({Key? key}) : super(key: key);
+
   @override
   State<SplashScreen> createState() => _SplashScreenState();
 }
